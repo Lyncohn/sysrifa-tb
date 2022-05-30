ea18e24cbed6337bc9c1f52d3f3eeea41b00f628
Lyncohn <lyncohn.macedoz@gmail.com>
2022-05-29 21:28:31 -0300

    Commit

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -3,7 +3,16 @@
     //Carregar as credenciais do banco de dados
 
     $hostname = "sql113.epizy.com";
-    $database = "";
+    $database = "epiz_31448286_sysrifa";
     $user = "epiz_31448286";
-    $password ="";
+    $password ="kNbByRv0aTHSmV";
+
+    try {
+        $pdo = new PDO('mysql:host=' .$hostname. ';dbname=' .$database, $user, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo 'Conexão com o banco de dados '.$database.', foi realizada com sucesso!';
+    } catch(PDOException $e) {
+        echo 'Erro: ' .$e->getMessage();
+    }
+
 ?>
