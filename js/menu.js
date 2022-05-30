83bdabbf4b0d8c47603c9c6ea42f3da6c456a5e5
Lyncohn <lyncohn.macedoz@gmail.com>
2022-05-29 21:47:11 -0300

    Commit

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    
-    $('nav-link').click(function(e) {
+
+    $('.nav-link').click(function(e) {
         e.preventDefault()
 
         alert('Você clicou em um Link!')
