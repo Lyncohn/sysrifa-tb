633ea74f3a1411143ba77a2aa52dd8e34ebd7253
Lyncohn <lyncohn.macedoz@gmail.com>
2022-06-01 21:09:25 -0300

    Commit

--- src/tipo/controle/novo-tipo.js
+++ src/tipo/controle/novo-tipo.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
-    
-    $('.btn-novo').click(function(e) {
+
+    $('.btn-novo').click(function(e){
         e.preventDefault()
 
-        alert('Você clicou no botão!!!')
+        //Limpar todas as informações já existentes em nossa modal
+        $('.modal-tipo').empty()
+        $('.modal-body').empty()
+
+        //Incluir novos textos no cabeçalho da minha janela modal
+        $('.modal-title').append('Adicionar novo registro')
+
+        //Incluir nosso formulário dentro do corpo da nossa janela modal
+        $('.modal-body').load('src/tipo/visao/form-tipo.html')
+
+        //Iremos incluir uma função no botão salvar para demonstrar que é um novo registro
+        $('.btn-salvar').attr('data-operation', 'insert-')
+
+        //Abrir a janela modal
+        $('.#modal-tipo').modal('show')
     })
+
 })
