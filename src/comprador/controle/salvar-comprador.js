5df5592631a55cb4723d7520e2f5eb9f5c1eb2ef
Lyncohn <lyncohn.macedoz@gmail.com>
2022-06-05 16:33:35 -0300

    Commit

---
+++ src/comprador/controle/salvar-comprador.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    $('.btn-salvar').click(function (e){
+        e.preventDefault()
+
+        let dados = $('#form-tipo').serialize()
+
+        dados += `&operacao=${$('.btn-salvar').attr('data-operation')}`
+    
+        $.ajax({
+            type: 'POST',
+            dataType: 'JSON',
+            assync: true,
+            data: dados,
+            url: 'src/comprador/modelo/salvar-comprador.php',
+            success: function(dados) {
+                Swal.fire({
+                    title: 'Sistemas de Rifas',
+                    text: dados.mensagem,
+                    icon: dados.tipo,
+                    confirmButtonText: 'OK'
+                })
+
+                $('#modal-tipo').modal('hide')
+            }
+        })
+
+    })
+
+})
